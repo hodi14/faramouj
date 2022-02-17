bec4bdbf923446e9b048c24b44f3d336938642f6
Hodi14 <alirezahodaee14@gmail.com>
2022-02-17 12:29:46 +0330

    fixed some bugs

--- Components/Footer/FooterContact.js
+++ Components/Footer/FooterContact.js
@@ -3,7 +3,7 @@ export default function FooterContact() {
     <div className="footerContact">
       <h4>تماس با فراموج</h4>
       <a href="https://maps.app.goo.gl/ygPBMKb1HHYkkQE68">
-        ایران: تهران، یوسف آباد، نبش خیابان مدبر، کوچه 28 ام، پلاک 64
+        ایران: تهران، یوسف آباد، قبل از خیابان مدبر، کوچه 28 ام، پلاک 64
       </a>
       <a href="tel:02188015422">تلفن: 02188015422</a>
     </div>
