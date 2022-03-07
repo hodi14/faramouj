6d9ddd58717bee222f1f82905a2d5649cab12e12
Hodi14 <alirezahodaee14@gmail.com>
2022-03-07 10:10:36 +0330

    added awards voting link

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,21 @@ import HomeCategories from "../Components/Home/HomeCategories";
 import HomeComments from "../Components/Home/HomeComments";
 import HomeDream from "../Components/Home/HomeDream";
 import axios from 'axios';
+import { useEffect } from "react";
 
 
 export default function Home({blogPosts}) {
+  useEffect(() => {
+    !(function (e, t, a) {
+      "use strict";
+      var s = t.head || t.getElementsByTagName("head")[0],
+        p = t.createElement("script");
+      (e.iwmfBadge = a),
+        (p.async = true),
+        (p.src = "https://cdn.iwmf.ir/js/people-votes/people-vote-4-2.js"),
+        s.appendChild(p);
+    })(window, document, "b-bottom-left");
+  })
   return (
     <section className="innerPage homePage">
 
